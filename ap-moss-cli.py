d2b809e4dc0afb6422f9897496cd2b7282a35cde
Danial Kh <17182126+dark-0ne@users.noreply.github.com>
2019-03-04 01:27:30 +0330

    Add download starter

--- ap-moss-cli.py
+++ ap-moss-cli.py
@@ -2,16 +2,39 @@
 import os
 import argparse
 import shutil
+import requests
+
+from github import Github
 
 
-def cleanup(wd):
-    shutil.rmtree(os.path.join(wd, 'repos'), ignore_errors=True)
-    shutil.rmtree(os.path.join(wd, 'out'), ignore_errors=True)
+def cleanup_dirs(wd, project_name):
+    shutil.rmtree(os.path.join(wd, 'repos', project_name), ignore_errors=True)
+    shutil.rmtree(os.path.join(wd, 'out', project_name), ignore_errors=True)
 
 
-def startup(wd):
-    os.makedirs(os.path.join(wd, 'repos'), exist_ok=True)
-    os.makedirs(os.path.join(wd, 'out'), exist_ok=True)
+def startup_dirs(wd, project_name):
+    os.makedirs(os.path.join(wd, 'repos', project_name), exist_ok=True)
+    os.makedirs(os.path.join(wd, 'out', project_name), exist_ok=True)
+
+
+def connect_github(token=None, username=None, pwd=None):
+    if token is None:
+        return Github(username, pwd)
+    else:
+        return Github(token)
+
+
+def download_starter(wd, project_name, g):
+
+    repo = g.get_repo("k-n-toosi-university-of-technology/" + project_name +
+                      '-starter')
+    src_files = repo.get_contents("src/main/java/ir/ac/kntu")
+
+    for src in src_files:
+        r = requests.get(src.download_url)
+        with open(os.path.join(wd, "repos", project_name, src.name),
+                  'wb') as f:
+            f.write(r.content)
 
 
 def main():
@@ -27,12 +50,9 @@ def main():
         "-u",
         "--username",
         nargs='?',
-        help="Github username. Can also be set via env variable AP_MOSS_USER")
-    parser.add_argument(
-        "-p",
-        "--password",
-        nargs='?',
-        help="Github password. Can also be set via env variable AP_MOSS_PWD")
+        help="Github username. If not provided will look for token in env" +
+        " variable AP_MOSS_TOKEN")
+    parser.add_argument("-p", "--password", nargs='?', help="Github password")
     parser.add_argument(
         "-o",
         "--output",
@@ -45,34 +65,28 @@ def main():
         nargs='?',
         default=False,
         const=True,
-        help="Clean up previous output files and repos")
+        help="Deletes previous output files and repos")
 
     args = parser.parse_args()
 
     # Check for username and pwd
     if args.username is None:
         try:
-            args.username = os.environ['AP_MOSS_USER']
+            args.token = os.environ["AP_MOSS_TOKEN"]
         except KeyError:
             print(
-                "Provide username either through parameteres or env variables."
+                "Please provide either username/pwd or auth token for github."
                 + " Terminating!")
-            raise SystemExit
-
-    if args.password is None:
-        try:
-            args.password = os.environ['AP_MOSS_PWD']
-        except KeyError:
-            print(
-                "Provide password either through parameteres or env variables."
-                + " Terminateing!")
             raise SystemExit
 
     # Cleanup and setup
     if args.force_cleanup:
-        cleanup(args.output)
+        cleanup_dirs(args.output, args.output)
 
-    startup(args.output)
+    startup_dirs(args.output, args.project)
+
+    download_starter(args.output, args.project,
+                     connect_github(args.token, args.username, args.password))
 
 
 if __name__ == '__main__':
